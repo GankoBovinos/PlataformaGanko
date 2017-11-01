e7743b209b2c143e0aa8ba7971a78ae903c74b23
Felipe Arcos <efarcos@unicauca.edu.co>
2017-11-01 12:13:56 -0500

    creado el modelo de Bovine

---
+++ app/src/main/java/ganko/movil/ganko1/data/model/Bovine.kt
@@ -0,0 +1,47 @@
+package ganko.movil.ganko1.data.model
+
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.Ignore
+import android.arch.persistence.room.PrimaryKey
+import java.util.*
+
+/**
+ * Created by Asus on 1/11/2017.
+ */
+
+@Entity
+data class Bovine(@PrimaryKey(autoGenerate = true) var _id:Long?,
+                  var id : Long?,
+                  var id_bovino: String,
+                  var imagen: String?,
+                  var name: String?,
+                  var fecha: Date,
+                  var genero: String,
+                  var proposito: String,
+                  var peso: Int,
+                  var color: String?,
+                  var raza: String?,
+                  var id_madre: String?,
+                  var id_padre: String?,
+                  var salida: String?,
+                  var lote: String?,
+                  var salida_por: String?,
+                  var numero_partos: Int?,
+                  var parto_fallo: String?,
+                  var fecha_salida: Date?,
+                  var finca: Int,
+                  var usuario: Int,
+                  var fecha_compra: Date?,
+                  var precio_compra : Int?,
+                  var precio_venta : Int?,
+                  var toSync : Boolean? = false
+                  )
+{
+    @Ignore
+    constructor(id_bovino: String, imagen: String?, name: String?, fecha: Date, genero: String, proposito: String, peso: Int,
+                color: String?, raza: String?, id_madre: String?, id_padre: String?, salida: String?, lote: String?,
+                salida_por: String?, numero_partos: Int?, parto_fallo: String?, fecha_salida: Date?, finca: Int, usuario: Int,
+                fecha_compra: Date?, precio_compra: Int?, precio_venta: Int?) : this (
+            null, null, id_bovino, imagen, name, fecha, genero, proposito, peso, color, raza, id_madre, id_padre, salida, lote,
+            salida_por, numero_partos, parto_fallo, fecha_salida, finca, usuario, fecha_compra, precio_compra, precio_venta, true )
+}
