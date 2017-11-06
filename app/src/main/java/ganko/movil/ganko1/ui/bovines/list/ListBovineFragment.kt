3849c24eb59ffa9568fc5cf4e890f9d16cfa7dca
Felipe Arcos <efarcos@unicauca.edu.co>
2017-11-06 16:47:54 -0500

    voy a hacer el pull de las dependicas

--- app/src/main/java/ganko/movil/ganko1/ui/bovines/list/ListBovineFragment.kt
+++ app/src/main/java/ganko/movil/ganko1/ui/bovines/list/ListBovineFragment.kt
@@ -1,11 +1,15 @@
 package ganko.movil.ganko1.ui.bovines.list
 
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.*
 
 import ganko.movil.ganko1.R
+import ganko.movil.ganko1.databinding.FragmentListBovineBinding
+import ganko.movil.ganko1.ui.adapters.BovineAdapter
+import javax.inject.Inject
 
 
 /**
@@ -13,15 +17,26 @@ import ganko.movil.ganko1.R
  */
 class ListBovineFragment : Fragment() {
 
+    @Inject
+    lateinit var adapter : BovineAdapter
+    lateinit var binding : FragmentListBovineBinding
+
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater!!.inflate(R.layout.fragment_list_bovine, container, false)
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_list_bovine, container, false)
+
+        return binding.root
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_bovine, menu)
+    }
+
+    companion object {
+        fun instance(): ListBovineFragment = ListBovineFragment()
     }
 
 }// Required empty public constructor
