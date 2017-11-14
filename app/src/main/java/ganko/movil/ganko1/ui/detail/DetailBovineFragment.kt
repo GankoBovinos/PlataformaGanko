614eb366a40297e0e1c78c14a5ba4c96bc26801e
Felipe Arcos <efarcos@unicauca.edu.co>
2017-11-13 23:08:44 -0500

    se hizo el data binding de detail, falta navegacion, aun no lo tengo muy claro

--- app/src/main/java/ganko/movil/ganko1/ui/detail/DetailBovineFragment.kt
+++ app/src/main/java/ganko/movil/ganko1/ui/detail/DetailBovineFragment.kt
@@ -1,6 +1,7 @@
 package ganko.movil.ganko1.ui.detail
 
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
@@ -8,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 
 import ganko.movil.ganko1.R
+import ganko.movil.ganko1.databinding.FragmentDetailBovineBinding
 
 
 /**
@@ -15,10 +17,16 @@ import ganko.movil.ganko1.R
  */
 class DetailBovineFragment : Fragment() {
 
+    lateinit var binding : FragmentDetailBovineBinding
+
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater!!.inflate(R.layout.fragment_detail_bovine, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_detail_bovine, container, false )
+        return binding.root
+    }
+
+    companion object {
+        fun instance(): DetailBovineFragment = DetailBovineFragment()
     }
 
 }// Required empty public constructor
