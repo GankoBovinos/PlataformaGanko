89162bb817136b91fedb706dd212304e9912b7a4
Felipe Arcos <pipearcos221@gmail.com>
2017-11-30 23:00:16 -0500

    se cambio el codigo implementado al layout list milk fragment, en la case se implemento el binding  y el adapter ademar de crear la instancia del fragment

--- app/src/main/java/ganko/movil/ganko1/ui/detail/milk/list/ListMilkFragment.kt
+++ app/src/main/java/ganko/movil/ganko1/ui/detail/milk/list/ListMilkFragment.kt
@@ -1,6 +1,7 @@
 package ganko.movil.ganko1.ui.detail.milk.list
 
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
@@ -8,18 +9,32 @@ import android.view.View
 import android.view.ViewGroup
 
 import ganko.movil.ganko1.R
+import ganko.movil.ganko1.data.model.Milk
+import ganko.movil.ganko1.databinding.FragmentListMilkBinding
+import ganko.movil.ganko1.di.Injectable
+import ganko.movil.ganko1.ui.adapters.MilkAdapter
+import javax.inject.Inject
 
 
 /**
  * A simple [Fragment] subclass.
  */
-class ListMilkFragment : Fragment() {
+class ListMilkFragment : Fragment(), Injectable {
 
+    @Inject
+    lateinit var adapter : MilkAdapter
+    lateinit var binding : FragmentListMilkBinding
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater!!.inflate(R.layout.fragment_list_milk, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_list_milk, container, false)
+        binding.recyclerMilk.adapter = adapter
+        return binding.root
+    }
+
+    companion object {
+        fun instance(milk:Milk) : ListMilkFragment = ListMilkFragment()
     }
 
 }// Required empty public constructor
