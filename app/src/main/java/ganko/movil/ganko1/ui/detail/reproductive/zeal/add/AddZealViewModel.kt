d6b00d903ed86a9fdfdd1404a9c208fe4fac820c
Dario Chamorro <dario.chamorro.v@gmail.com>
2017-12-13 16:47:20 -0500

    Detail Module, actualizado sin ListBovineFragment

--- app/src/main/java/ganko/movil/ganko1/ui/detail/reproductive/zeal/add/AddZealViewModel.kt
+++
@@ -1,9 +0,0 @@
-package ganko.movil.ganko1.ui.detail.reproductive.zeal.add
-
-import android.support.v7.app.AppCompatActivity
-
-/**
- * Created by jlbeltran94 on 13/12/2017.
- */
-class AddZealViewModel: AppCompatActivity() {
-}
